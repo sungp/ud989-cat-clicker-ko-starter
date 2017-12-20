1ffff05ba93e20ffc9c93e33c3d624e10026c93c
Sung <sungp2k@gmail.com>
2017-12-20 06:33:40 -0800

    cat level added

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var ViewModel = function() {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Tabby');
+    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
+    this.level = ko.computed(function() {
+        var count = this.clickCount();
+        if (count == 0) {
+            return "Newborn";
+        } 
+        if (count < 10) {
+            return "Infant";
+        }
+        if (count < 100) {
+            return "Teen";
+        }
+        return "Adult";
+    }, this);
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount() + 1);
+    };
+}
+
+ko.applyBindings(new ViewModel());
